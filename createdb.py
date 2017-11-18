07baaf5ac497d92526c41b37a28ff1c26ed35f05
gripleaf <425797155@qq.com>
2017-11-18 23:25:52 +0800

    allok

---
+++ createdb.py
@@ -0,0 +1,30 @@
+from app import db
+# from models import BlogPost
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+
+
+# create the database and the db table
+db.create_all()
+
+# insert data
+db.session.add(User("Good", "I\'m good."))
+db.session.add(User("Well", "I\'m well."))
+# db.session.add(BlogPost("Excellent", "I\'m excellent."))
+# db.session.add(BlogPost("Okay", "I\'m okay."))
+# db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
+
+# commit the changes
+db.session.commit()
