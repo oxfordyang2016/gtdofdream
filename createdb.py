3da265354efdfa2d2eab1d0b04da442e54fc83a2
gripleaf <425797155@qq.com>
2017-11-18 23:30:07 +0800

    allok

--- createdb.py
+++ createdb.py
@@ -1,7 +1,7 @@
 from app import db
 # from models import BlogPost
 
-class User(db.Model):
+class User1(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
@@ -20,8 +20,8 @@ class User(db.Model):
 db.create_all()
 
 # insert data
-db.session.add(User("Good", "I\'m good."))
-db.session.add(User("Well", "I\'m well."))
+db.session.add(User1("Good", "I\'m good."))
+db.session.add(User1("Well", "I\'m well."))
 # db.session.add(BlogPost("Excellent", "I\'m excellent."))
 # db.session.add(BlogPost("Okay", "I\'m okay."))
 # db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
