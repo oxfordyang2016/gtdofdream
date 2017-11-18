2c28c409e4730b25cf407c2d7837112c56dd6b00
gripleaf <425797155@qq.com>
2017-11-18 21:03:34 +0800

    allok

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
-
+db.create_all()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
