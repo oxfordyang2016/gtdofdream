afa86adbe5683f36b5ef791f9ceb9ac205aa4baf
gripleaf <425797155@qq.com>
2017-11-19 16:28:34 +0800

    allok

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os,time,json
+import os,time,json,jsonify
 from flask import Flask, request
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -104,6 +104,25 @@ def information():
         return json.dumps({'content':str(content),'time':str(time.time())})
   
 
+@app.route('/info1',methods=['GET','POST'])
+def information1():
+    if request.method == 'POST':
+        content = request.values.get('input')
+        #user = User(str(time.time()), str(content)+str(time.time()))
+        #db.session.add(user)
+        taskcontent = task(str(content))
+        db.session.add(taskcontent)
+        db.session.commit()
+        print(content)
+        #all_users = User.query.all()
+        #print(all_users)
+        '''
+        for k in all_users:
+            print(str(k.email)+'email')
+        '''    
+        #content = task(content)
+        #print(content)
+        return jsonify("input":str(content))
 
 
 
