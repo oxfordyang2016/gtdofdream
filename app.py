7bd8f9bedd20b4beb261e154b4e288e1b26b0a74
RCY-FUDAN <rcy-fudan@ioeb-FUDANdeMacBook-Pro.local>
2017-12-01 23:25:09 +0800

    fix gtd-cli function

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def gtd-cli():
     print("i have no word")
     if request.method == 'POST':
         inboxthing = request.values.get('inbox')
-        input-time = request.values.get('input-time'))
-        task-status = request.values.get('task-status')
+        inputtime = request.values.get('input-time'))
+        taskstatus = request.values.get('task-status')
         plantime = request.values.get('plantime')
         print(inboxthing)
         return json.dumps({'status':'u have uploaded successfully'})
